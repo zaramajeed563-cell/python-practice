c8d2e847b903320ec3f4bedf0a696a62f01d1e84
zaramajeed563-cell <zaramajeed563@gmail.com>
2025-11-14 17:25:59 +0500

    day11

--- python-practice/day11-list.py
+++ python-practice/day11-list.py
@@ -1,10 +1,10 @@
-""" #max number in list
+#max number in list
 numbers=[1,2,3,4,5]
 max=numbers[0]
 for number in numbers:
     if number>max:
         max=number
-print(max) """
+print(max)
 
 #Create a list of 5 numbers and print each one
 numbers=[4,5,6,7,8]
@@ -29,3 +29,19 @@ print(pets)
 cities = ["Lahore", "Karachi", "Islamabad"]
 cities.pop(0)
 print(cities)
+
+#Print all even numbers from the list
+nums = [10, 21, 4, 45, 66, 93]
+for number in nums:
+    if number%2==0:
+        print(number)
+
+#Find the largest number
+marks = [45, 12, 89, 33, 50]
+max=marks[0]
+for mark in marks:
+    if mark>max:
+        max=mark
+print(max)
+
+#Reverse the list WITHOUT using reverse()
