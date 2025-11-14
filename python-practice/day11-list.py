59f84338443d05eed0a6bbe4491bec7115b20700
zaramajeed563-cell <zaramajeed563@gmail.com>
2025-11-14 17:15:49 +0500

    day11

--- python-practice/day11-list.py
+++ python-practice/day11-list.py
@@ -1,8 +1,31 @@
-#max number in list
+""" #max number in list
 numbers=[1,2,3,4,5]
 max=numbers[0]
 for number in numbers:
     if number>max:
         max=number
-print(max)
+print(max) """
 
+#Create a list of 5 numbers and print each one
+numbers=[4,5,6,7,8]
+print(numbers)
+
+#Print only the first and last element
+fruits = ["apple", "banana", "mango", "kiwi", "orange"]
+print(fruits[0])
+print(fruits[-1])
+
+#Change the second element into something else
+colors = ["red", "blue", "green"]
+colors[1]="purple"
+print(colors)
+
+#Add a new item to the list
+pets = ["cat", "dog"]
+pets.append("goat")
+print(pets)
+
+#Remove an item from the list
+cities = ["Lahore", "Karachi", "Islamabad"]
+cities.pop(0)
+print(cities)
