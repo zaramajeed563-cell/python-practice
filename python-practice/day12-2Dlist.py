1784da9754a1747758aae545817485a2cdf030a5
zaramajeed563-cell <zaramajeed563@gmail.com>
2025-11-20 16:58:32 +0500

    day12

--- python-practice/day12-2Dlist.py
+++ python-practice/day12-2Dlist.py
@@ -41,3 +41,62 @@ A = [
 ]
 for row in A:
     print(row[-1])
+
+#For each row, print all elements on the same line.
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for row in A:
+    for i in row:
+        print(i,end=" ")
+
+#Count how many numbers are in the 2D list.
+count=0
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for row in A:
+    count+=len(row)
+print(count)
+
+#Calculate the sum of all numbers in the 2D list.
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+sum=0
+for row in A:
+    for i in row:
+        sum+=i
+print(f"sum is {sum}")
+
+#Find the largest number in the 2D list.
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+max=A[0][1]
+for i in A:
+    for y in i:
+     if y>=max:
+        max=y
+print(max)
+
+#Find the smallest number in the 2D list.
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+min=A[0][1]
+for i in A:
+    for y in i:
+     if y<=min:
+        min=y
+print(min)
