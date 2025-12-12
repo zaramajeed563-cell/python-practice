4808ebcd8fc85996d6dc5e557a5b419fb0d83f6d
zaramajeed563-cell <zaramajeed563@gmail.com>
2025-12-12 17:05:06 +0500

    module testing

---
+++ python-practice/modules/mathops/main.py
@@ -0,0 +1,4 @@
+import math_ops
+print(math_ops.square(4))
+print(math_ops.cube(2))
+
---
+++ python-practice/modules/mathops/math_ops.py
@@ -0,0 +1,11 @@
+""" Create a module named math_ops.py with functions:
+square(n)
+cube(n)
+Import it in another file and use them. """
+def square(n):
+    result=n*n
+    return f"The square is {result}"
+def cube(n):
+    result=n*n*n
+    return f"the cube is {result}"
+
